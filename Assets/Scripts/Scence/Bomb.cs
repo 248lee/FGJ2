64a6577bd72a2cf5fe1483c18475de846c1f7f20
bootcat123shecat123 <oooth16@gmail.com>
2023-09-03 01:58:29 +0800

    littie find player

--- Assets/Scripts/Scence/Bomb.cs
+++ Assets/Scripts/Scence/Bomb.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class Bomb : MonoBehaviour
@@ -11,7 +10,7 @@ public class Bomb : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        player=FindObjectOfType<PlayerController>().GameObject().GetComponent<PlayerController>();
+        player=FindObjectOfType<PlayerController>();
         Bombed();
     }
 
