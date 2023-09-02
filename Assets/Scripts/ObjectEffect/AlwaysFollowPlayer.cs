b207d8aa4d6c978e4e63cf4ccaa860ff0f761bf8
bootcat123shecat123 <oooth16@gmail.com>
2023-09-02 20:14:10 +0800

    Follow player

---
+++ Assets/Scripts/ObjectEffect/AlwaysFollowPlayer.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlwaysFollowPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] PlayerController player;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position; 
+    }
+}
